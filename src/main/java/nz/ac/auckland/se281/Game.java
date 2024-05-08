a41763787af58734d2368df0e5f91c5e50bcaf33
Beren Eskildsen <besk864@aucklanduni.ac.nz>
2024-05-08 20:19:34 +1200

    Implementing some of the play logic by showing current round number

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,15 +6,32 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  RoundSet currentSet;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+
+    currentSet = new RoundSet();
   }
 
-  public void play() {}
+  public void play() {
 
-  public void endGame() {}
+    currentSet.incrementRoundNumber();
+
+    MessageCli.START_ROUND.printMessage(String.valueOf(currentSet.getRoundNumber()));
+
+
+  }
+
+  public void endGame() {
+
+
+
+
+
+    currentSet = null;
+  }
 
   public void showStats() {}
 }
